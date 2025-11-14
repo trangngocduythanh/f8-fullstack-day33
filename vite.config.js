90f8964ab78114e7240b4c1ad96b715f1dc98b28
trangngocduythanhh <trangngocduythanhh@example.com>
2025-11-14 22:25:28 +0700

    Sửa vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/f8-fullstack-day33/",
     resolve: {
         alias: {
             // eslint-disable-next-line no-undef
